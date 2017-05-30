03cfb4578d861e88a5862e73eddddd45118b3a79
Pasquale CS <info@pasqualecappaspina.com>
2017-05-30 15:30:26 +0200

    Master

--- js/pcsLiveSearch.js
+++ js/pcsLiveSearch.js
@@ -11,36 +11,44 @@
     $.fn.pcsLiveSearch = function( options ) {
     	var el = this;
     	var selector = el.selector;
+    	var timer = 0;
 
         // Default options.
         var settings = $.extend({
             // These are the defaults.
+            timePause: 500,
+            minChSearch: 2
         }, options );
 
         // Search
 		$(el).keyup(function() {
+			clearTimeout(timer);
+
 			var search = $(this);
 			var value = $(this).val();
 			var id_box = 'pcs-live-search-box';
 
 			$('.search-results').remove();
 
-			var box = '<ul id="' + id_box + '" class="search-results">';
+			if(value.length > settings.minChSearch) {
+				timer = setTimeout(function() {
+					var box = '<ul id="' + id_box + '" class="search-results">';
 
-			$.getJSON( settings.file, { search: value })
-			  	.done(function( data ) {
-				  	$.each(data, function(i, r) {
-				  		if(r[settings.print] !== null) {
-				  			box += '<li onclick="pcsSelectValue(\'' + selector + '\', \'' + r[settings.print] + '\')">' + r[settings.print] + '</li>';
-				  		}
+					$.getJSON( settings.file, { search: value })
+					  	.done(function( data ) {
+						  	$.each(data, function(i, r) {
+						  		if(r[settings.print] !== null) {
+						  			box += '<li onclick="pcsSelectValue(\'' + selector + '\', \'' + r[settings.print] + '\')">' + r[settings.print] + '</li>';
+						  		}
+							});
+
+						box += '</ul>';
+
+						search.after(box);
 					});
-
-				box += '</ul>';
-
-				search.after(box);
-			});
+				}, settings.timePause);
+			}
 		});
-        
     };
  
 }( jQuery ));
